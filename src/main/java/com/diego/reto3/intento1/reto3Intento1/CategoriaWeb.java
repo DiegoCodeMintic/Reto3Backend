699b0872f5b4d2d0871651576807668e99301235
Diego Bernal <diegobernal8810@hotmail.com>
2021-10-19 09:23:26 -0500

    pre publicacion

--- src/main/java/com/diego/reto3/intento1/reto3Intento1/CategoriaWeb.java
+++ src/main/java/com/diego/reto3/intento1/reto3Intento1/CategoriaWeb.java
@@ -6,13 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 /*
@@ -28,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/Category")
 public class CategoriaWeb {
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/holaMundo")
     public String saludar(){
         return "Hola Mundo Colombia from server diego R";
